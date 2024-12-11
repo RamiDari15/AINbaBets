3b81cce76586e7ce5e858a8569f1a0099872b071
Rami Dari <145464625+rdari5@users.noreply.github.com>
2024-12-10 20:08:06 -0500

    Delete:

--- ai_stat_checker/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.aistatchecker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ai_stat_checker/lib/index.dart
+++
@@ -1,3 +0,0 @@
-// Export pages
-export '/home_page/home_page_widget.dart' show HomePageWidget;
-export '/home_paged/home_paged_widget.dart' show HomePagedWidget;
